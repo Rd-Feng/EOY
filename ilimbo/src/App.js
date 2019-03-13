03c0e5f9b9cd933a7d45ebb18ea31c38bb2f5e03
Rd-Feng <394@holbertonschool.com>
2019-03-13 10:14:25 -0700

    indentation

--- ilimbo/src/App.js
+++ ilimbo/src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './Models/landing_page'
 
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-                <div className="main">
-                    <Route exact={true} path='/' render={() => (
-                        <div>
-                            <div className="content">
-                                <Landing  />
-                            </div>
-                        </div>
-                    )} />
+        <div className="main">
+            <Route exact={true} path='/' render={() => (
+                <div>
+                    <div className="content">
+                        <Landing  />
+                    </div>
                 </div>
-            </BrowserRouter>
+            )} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
