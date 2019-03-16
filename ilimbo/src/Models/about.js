e48ad16db28776c4efcf03e71f9bee4126b9c8fe
sumin3 <suminyu@yahoo.com>
2019-03-16 10:34:31 -0700

    add animation

--- ilimbo/src/Models/about.js
+++ ilimbo/src/Models/about.js
@@ -10,7 +10,7 @@ class About extends Component {
     this.handleScroll = this.handleScroll.bind(this);
   }
   handleScroll() {
-    if (window.pageYOffset > 500) {
+    if (window.pageYOffset > 400) {
       this.setState({ isScroll: true })
     }
     else {
@@ -26,26 +26,29 @@ class About extends Component {
 
   render() {
     return (
-        <div className="about-component">
-            <div className="developers"> Developers </div>
-            <div className="meet-the-team">
-                <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
-                    Sumin Yu
-                    <img src={require('./team/sumin.jpg')}/>
-                </div>
-                <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
-                    Rui Feng
-                    <img src={require('./team/rui.JPG')}/>
-                </div>
-                <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
-                    Jian Huang
-                    <img src={require('./team/jian.png')}/>
-                </div>
+      <div className="about-component">
+        <div className="app-description">
+        <p className="about-invisible">about us invisible text for scrolling down</p>
+        <h1>Mission</h1>
+         <p> This application was created with the intent of bringing developers together by offering a common topic of discussion. Here at Limbo, we believe less is more. Users will be given a popular article sourced from HackerNews. From there, users can choose to read, comment, and connect with other users to expand their professional network. Enjoy.</p>
             </div>
-            <div className="app-description">
-                This application was created with the intent of bringing developers together by offering a common topic of discussion. Here at Limbo, we believe less is more. Users will be given a popular article sourced from HackerNews. From there, users can choose to read, comment, and connect with other users to expand their professional network. Enjoy.
-            </div>
+        <div className="developers"> Developers </div>
+        <div className="meet-the-team">
+          <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
+            <h4>Sumin Yu</h4>
+            <img src={require('./team/sumin.jpg')} />
+
+          </div>
+          <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
+            <h4>Rui Feng</h4>
+            <img src={require('./team/rui.JPG')} />
+          </div>
+          <div className={(this.state.isScroll) ? "team-card-animated" : "team-card"}>
+            <h4>Jian Huang</h4>
+            <img src={require('./team/jiancopy.jpg')} />
+          </div>
         </div>
+      </div>
     )
   }
 }
--- ilimbo/src/Models/landing_page.js
+++ ilimbo/src/Models/landing_page.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './styles/landing_navbar.css'
 import LandingHeader from './landing_header'
 import About from './about'
+import InfiniteScroll from 'react-infinite-scroller';
 
 
 class Landing extends Component {
@@ -10,30 +11,7 @@ class Landing extends Component {
       <div>
         <section id="intro"></section>
         <LandingHeader />
-        <section id='about'> <About/> </section>
-        <p>sds</p>
-        <p>sds</p>
-
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
-        <p>sds</p>
+        <section id='about'> <About /> </section>
         <section id='article' className="landing-article">
           <h2>Article:</h2>
           <p>placeholder for comment box</p>
@@ -49,7 +27,14 @@ class Landing extends Component {
           <p>placeholder for comment box</p>
           <p>placeholder for comment box</p>
           <p>placeholder for comment box</p>
-          <p>placeholder for comment box</p>
+          {/* <InfiniteScroll
+            pageStart={0}
+            loadMore={loadFunc}
+            hasMore={true || false}
+            loader={<div className="loader" key={0}>Loading ...</div>}
+          >
+            {items}
+          </InfiniteScroll> */}
         </section>
       </div>
     )
