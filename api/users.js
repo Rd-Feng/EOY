20c0cb60ec82303e0c118e166e095e59cab16e97
Rd-Feng <394@holbertonschool.com>
2019-03-18 12:29:42 -0700

    get and post

---
+++ api/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+const connection = (() => {
+  return mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'limbo_test',
+    connectionLimit: 100
+  });
+})();
+
+router.get('/user/:id', (req, res) => {
+  connection.query('SELECT * FROM users WHERE id=' + connection.escape(req.params.id), (err, results) => {
+    if (err) {
+      console.log(err);
+      res.send({status: 'failed'});
+    } else {
+      return res.json({data: results});
+    }
+  });
+});
+
+router.post('/user', (req, res) => {
+  const { id, first_name, last_name, email, img_url } = req.body;
+  connection.query('INSERT INTO users (id, first_name, last_name, email, img_url) VALUES (' +
+                    connection.escape(id) + ', ' +
+                    connection.escape(first_name) + ', ' +
+                    connection.escape(last_name) + ', ' +
+                    connection.escape(email) + ', ' +
+                    connection.escape(img_url) + ')', (err, results) => {
+    if (err) {
+      console.log(err);
+      res.send({status: 'failed'});
+    } else {
+      return res.json({status: 'success'});
+    }
+  });
+});
+
+module.exports = router;
