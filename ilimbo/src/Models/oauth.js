1d3e924d816995c6b21549e1a1fea33aca41407b
Rd-Feng <394@holbertonschool.com>
2019-03-18 09:33:43 -0700

    backend verification

--- ilimbo/src/Models/oauth.js
+++ ilimbo/src/Models/oauth.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
-import GoogleLogin from 'react-google-login';
+import GoogleLogin from 'react-google-login'
+import {withRouter} from 'react-router-dom';
 
 
 class OAuth extends Component {
   render() {
     const responseGoogle = (response) => {
-      let profile = response.getBasicProfile();
-      console.log(response.getAuthResponse().id_token);
-      localStorage.setItem("id_token", JSON.stringify(response.getAuthResponse().id_token));
-      localStorage.setItem("ImgUrl", JSON.stringify(profile.getImageUrl()));
-      localStorage.setItem("Fullname", JSON.stringify(profile.getName()));
+      if (response.error) {
+        return;
+      }
+      fetch('http://localhost:4000/verify', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          token: response.getAuthResponse().id_token
+        })
+      })
+        .then(response => response.json())
+        .then(response => {
+          localStorage.setItem("id_token", JSON.stringify(response.id));
+          localStorage.setItem("ImgUrl", JSON.stringify(response.img_url));
+          localStorage.setItem("Fullname", JSON.stringify(response.first_name + ' ' + response.last_name));
+        })
     }
     return (
       <GoogleLogin
@@ -22,4 +37,4 @@ class OAuth extends Component {
   }
 }
 
-export default OAuth;
+export default withRouter(OAuth);
