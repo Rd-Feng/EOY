de33276b7be53427bfdd8b7a9f384c26d82f296c
Rd-Feng <394@holbertonschool.com>
2019-03-18 10:42:14 -0700

    get new article every min

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const schedule = require('node-schedule');
 const {OAuth2Client} = require('google-auth-library');
+const request = require('request');
 const CLIENT_ID = '959614478231-rhsbohn77k2664h64phq1v128lqp78l9.apps.googleusercontent.com'; /* TODO: put it as env */
 const client = new OAuth2Client(CLIENT_ID);
+let ARTICLE_TODAY = '19420532';
 
 const app = express();
 const PORT = process.env.PORT || 4000
@@ -23,11 +26,42 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+const TEST = schedule.scheduleJob('0 * * * * *', () => {
+  const GET_PAST_ITEMS = 'SELECT id FROM items';
+  connection.query(GET_PAST_ITEMS, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let history = [];
+      results.map(result => history.push(result.id));
+      request('https://hacker-news.firebaseio.com/v0/topstories.json', (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          let top_items = JSON.parse(body);
+          for (let item of top_items) {
+            if (!history.includes(String(item))) {
+              connection.query('INSERT INTO items (id) VALUES (' + connection.escape(String(item)) + ')', (err, results) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  ARTICLE_TODAY = String(item);
+                }
+              });
+              break;
+            }
+          }
+        }
+      });
+    }
+  })
+});
+
 app.post('/verify', (req, res) => {
   const { token } = req.body;
   client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID
+    idToken: token,
+    audience: CLIENT_ID
   })
   .then(ticket => data = ticket.getPayload())
   .then(data => data = {
@@ -56,9 +90,13 @@ app.get('/users', (req, res) => {
   });
 });
 
+app.get('/article_today', (req, res) => {
+  res.send({data: ARTICLE_TODAY});
+})
+
 app.get('/items', (req, res) => {
-  const GET_USERS = 'SELECT * FROM items';
-  connection.query(GET_USERS, (err, results) => {
+  const GET_ITEMS = 'SELECT * FROM items';
+  connection.query(GET_ITEMS, (err, results) => {
     if (err) {
       res.send({status: 'failed'});
     } else {
@@ -68,8 +106,8 @@ app.get('/items', (req, res) => {
 });
 
 app.get('/bookmarks', (req, res) => {
-  const GET_USERS = 'SELECT * FROM bookmarks';
-  connection.query(GET_USERS, (err, results) => {
+  const GET_BOOKMARKS = 'SELECT * FROM bookmarks';
+  connection.query(GET_BOOKMARKS, (err, results) => {
     if (err) {
       res.send({status: 'failed'});
     } else {
@@ -79,8 +117,8 @@ app.get('/bookmarks', (req, res) => {
 });
 
 app.get('/comments', (req, res) => {
-  const GET_USERS = 'SELECT * FROM comments';
-  connection.query(GET_USERS, (err, results) => {
+  const GET_COMMENTS = 'SELECT * FROM comments';
+  connection.query(GET_COMMENTS, (err, results) => {
     if (err) {
       res.send({status: 'failed'});
     } else {
@@ -90,8 +128,8 @@ app.get('/comments', (req, res) => {
 });
 
 app.get('/subcomments', (req, res) => {
-  const GET_USERS = 'SELECT * FROM subcomments';
-  connection.query(GET_USERS, (err, results) => {
+  const GET_SUBCOMMENTS = 'SELECT * FROM subcomments';
+  connection.query(GET_SUBCOMMENTS, (err, results) => {
     if (err) {
       res.send({status: 'failed'});
     } else {
@@ -101,8 +139,8 @@ app.get('/subcomments', (req, res) => {
 });
 
 app.get('/connections', (req, res) => {
-  const GET_USERS = 'SELECT * FROM connections';
-  connection.query(GET_USERS, (err, results) => {
+  const GET_CONNECTIONS = 'SELECT * FROM connections';
+  connection.query(GET_CONNECTIONS, (err, results) => {
     if (err) {
       res.send({status: 'failed'});
     } else {
