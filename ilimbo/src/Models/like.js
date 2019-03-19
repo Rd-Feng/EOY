129ad353a6c134a423477c7dfddcb3d273fcda46
sumin3 <suminyu@yahoo.com>
2019-03-19 14:33:51 -0700

    add alert message

--- ilimbo/src/Models/like.js
+++ ilimbo/src/Models/like.js
@@ -12,44 +12,49 @@ class Like extends Component {
       comment_id: this.props.comment_id
     }
   }
-  
+
   handleLike() {
-    this.setState({ check_like: !this.state.check_like });
-    let numLikes = this.state.likes
-    if (!this.state.check_like) {
-      numLikes++
-      this.setState({ like_color: { 'color': 'red' }, likes: numLikes});
-      this.incrNumLikes();
+    if (JSON.parse(localStorage.getItem("id_token"))) {
+      this.setState({ check_like: !this.state.check_like });
+      let numLikes = this.state.likes
+      if (!this.state.check_like) {
+        numLikes++
+        this.setState({ like_color: { 'color': 'red' }, likes: numLikes });
+        this.incrNumLikes();
+      }
+      else {
+        numLikes--
+        this.setState({ like_color: { 'color': '#A6A6A6' }, likes: numLikes });
+        this.decrNumLikes();
+      }
     }
     else {
-      numLikes--
-      this.setState({ like_color: { 'color': '#A6A6A6' }, likes: numLikes });
-      this.decrNumLikes();
+      alert("Login to Like the comment")
     }
   }
   incrNumLikes() {
     let commentType = this.props.isSubcomment ? "subcomment" : "comment";
-    fetch('http://localhost:4000/' + commentType + '/'+ this.state.comment_id + '/like')
-    .then(res => res.json())
-    .then(res => {
-      console.log(res);
-    })
+    fetch('http://localhost:4000/' + commentType + '/' + this.state.comment_id + '/like')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      })
   }
 
-  decrNumLikes(){
+  decrNumLikes() {
     let commentType = this.props.isSubcomment ? "subcomment" : "comment";
-    fetch('http://localhost:4000/'+ commentType + '/'+ this.state.comment_id + '/unlike')
-    .then(res => res.json())
-    .then(res => {
-      console.log(res);
-    })
+    fetch('http://localhost:4000/' + commentType + '/' + this.state.comment_id + '/unlike')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      })
   }
 
   render() {
     return (
       <i
         style={this.state.like_color}
-        onClick={() => {this.handleLike(); }}
+        onClick={() => { this.handleLike(); }}
       >&hearts; {this.state.likes}</i>
     )
   }
