06fead7346d54db580c48ee01c18a7f4772a6a48
TrieToSucceed <jian.huang.8581@gmail.com>
2019-03-19 08:41:10 +0000

    add route to bookmark page

--- ilimbo/src/App.js
+++ ilimbo/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './Models/landing_page'
 import Profile from './Models/profile/profile'
-
+import Bookmark from './Models/bookmark'
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="main">
           <Route exact={true} path='/' component={Landing} />
+	  <Route exact={true} path='/bookmark' component={Bookmark} />
         </div>
       </BrowserRouter>
     );
