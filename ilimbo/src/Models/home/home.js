53ff095b22f662fe2a756acaff26045968dcb125
sumin3 <suminyu@yahoo.com>
2019-03-19 16:40:20 -0700

    get article  id outside of comment component

--- ilimbo/src/Models/home/home.js
+++ ilimbo/src/Models/home/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import HomeHeader from './header'
 import './styles/home.css'
-// import Comment from './comment'
+import Comment from '../comment'
 
 
 class Home extends Component {
@@ -10,11 +10,28 @@ class Home extends Component {
     this.state = {     
     }
   }
- 
+  componentDidMount() {
+    fetch('http://localhost:4000/article_today')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ article_id: response.data})
+        fetch('https://hacker-news.firebaseio.com/v0/item/' + response.data + '.json')
+          .then(response => response.json())
+          .then(data => {
+            this.setState({ article_url: data.url, article_title: data.title });
+          })
+      })
+  }
   render() {
     return (
       <div >
-        <HomeHeader/>
+        <section><HomeHeader/></section>
+        <section className="home_section">
+        <Comment
+        article_url = {this.state.article_url}
+        article_title = {this.state.article_title}
+        article_id = {this.state.article_id}
+        /></section>
         </div>
     )
   }
