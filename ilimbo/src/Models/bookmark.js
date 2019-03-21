648d26d2faf60d6b3638ee2908aab3322694c35a
TrieToSucceed <jian.huang.8581@gmail.com>
2019-03-21 01:13:33 +0000

    implement conditional rendering of remove button

--- ilimbo/src/Models/bookmark.js
+++ ilimbo/src/Models/bookmark.js
@@ -59,16 +59,27 @@ class Bookmark extends Component {
     if (this.state.title) {
       cards = this.state.title.map((bookmark, index) => {
         let id = Object.keys(bookmark)[0]
-	console.log(this.state.bookmarks[index])
-	return (
-  	  <div class="card">
-          <div class="card-details">
-            <h2 class="card-head"> {bookmark[id]} </h2>
-            <a href="#/" class="card-action-button" onClick={(e) => {this.removeBookmark(e.target.id);}} id={id} >REMOVE</a>
-            <a href="#/" class="card-action-button" onClick={() => {this.linkHandler(id);}}>READ</a>
-          </div>
-        </div>
-        )
+        if (this.props.match.params.user_id === this.state.user_id) {	
+	  return (
+	    <div class="card">
+	    <div class="card-details">
+	      <h2 class="card-head"> {bookmark[id]} </h2>
+	      <a href="#/" class="card-action-button" onClick={(e) => {this.removeBookmark(e.target.id);}} id={id} >REMOVE</a>
+	      <a href="#/" class="card-action-button" onClick={() => {this.linkHandler(id);}}>READ</a>
+	    </div>
+	  </div>
+	  )
+	}
+        else {
+          return (
+	    <div class="card">
+	    <div class="card-details">
+	      <h2 class="card-head"> {bookmark[id]} </h2>
+	      <a href="#/" class="card-action-button" onClick={() => {this.linkHandler(id);}}>READ</a>
+	    </div>
+	  </div>
+	  )
+        }
       })
     }
     if (cards.length === 0) {
